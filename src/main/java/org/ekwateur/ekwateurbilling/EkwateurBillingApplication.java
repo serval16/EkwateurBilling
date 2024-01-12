34aafef4d0998e5f0f677846326aeaf8c732bd12
akbarshazad <akbarshazad@gmail.com>
2024-01-12 21:22:19 +0100

    first commit

---
+++ src/main/java/org/ekwateur/ekwateurbilling/EkwateurBillingApplication.java
@@ -0,0 +1,13 @@
+package org.ekwateur.ekwateurbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwateurBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkwateurBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ekwateur/ekwateurbilling/EkwateurBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ekwateur.ekwateurbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkwateurBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
